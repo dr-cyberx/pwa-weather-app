1591dffafaf9717b280694cb5c7962a9051f3492
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-09-28 11:59:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -30,6 +30,20 @@ const App = () => {
             <span>{weather.name}</span>
             <sup>{weather.sys.country}</sup>
           </h2>
+
+          <div className='city-temp'>
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+
+          <div className='info'>
+            <img
+              className='city-icon'
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt='weather Icon'
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
         </div>
       )}
     </div>
