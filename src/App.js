8487ad75dd0c1679f1ec09640a6a3583fddb9e82
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-09-28 11:44:22 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
   const search = async (e) => {
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
       console.log(data);
+      setWeather(data);
     }
   };
   return (
@@ -21,6 +23,15 @@ const App = () => {
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
+
+      {weather.main && (
+        <div className='city'>
+          <h2 className='city-name'>
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
